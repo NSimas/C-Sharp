36f43c99962543633ca74e7212028e3668d0a389
Nélia Simas <neliasimas@hotmail.com>
2022-11-20 03:04:24 -0300

    add

---
+++ CalcularMedia0001/Program.cs
@@ -0,0 +1,34 @@
+﻿public class CalcularMedia0001
+{
+    static void Main(string[] args)
+    {
+        float media = 0;
+
+        Console.WriteLine("Bem-vindo ao cálculo de média!");
+        Console.WriteLine("Vamos calcular a média do aluno!");
+
+        Console.WriteLine("Digite a primeira nota: ");
+        int.TryParse(Console.ReadLine(), out int nota1);
+
+        Console.WriteLine("Digite a segunda nota: ");
+        int.TryParse(Console.ReadLine(), out int nota2);
+
+        Console.WriteLine("Digite a terceira nota: ");
+        int.TryParse(Console.ReadLine(), out int nota3);
+
+        Console.WriteLine("Digite a quarta nota: ");
+        int.TryParse(Console.ReadLine(), out int nota4);
+
+        media = (nota1+nota2+nota3+nota4) /4;
+        Console.WriteLine("A média do aluno foi: {0}", media);
+
+        if(media <7)
+        {
+            Console.WriteLine("O aluno está em recuperação.");
+        }
+        else
+        {
+            Console.WriteLine("Aluno aprovado!");
+        }
+    }
+}
