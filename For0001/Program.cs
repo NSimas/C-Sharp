b110419b66a4fc931adb8b2f06445f4b50969a5d
Nélia Simas <neliasimas@hotmail.com>
2022-11-20 01:44:13 -0300

    add

---
+++ For0001/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var n1 = 1;
+        // estrutura de repetição/laço/loop FOR
+        for (n1 = 1; n1 <= 25; n1++)
+        {
+            Console.WriteLine(n1);
+        }
+    }
+}
