63ef2d8592fcade7eb61200297ef71bd3b3f4c94
Nélia Simas <neliasimas@hotmail.com>
2022-11-24 21:51:50 -0300

    adds

---
+++ 0002/Program.cs
@@ -0,0 +1,9 @@
+﻿int idadeUsuario = 20;
+if (idadeUsuario > 17)
+{
+    Console.WriteLine("Você já tem idade suficiente para tirar sua habilitação =}");
+}
+else
+{
+    Console.WriteLine("Você NÃO tem idade suficiente para tirar sua habilitação =/");
+}
