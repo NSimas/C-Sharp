b128099449f78d25567a5306be1762d9edab76fb
Nélia Simas <neliasimas@hotmail.com>
2023-01-08 21:20:38 -0300

    adds

---
+++ Caneta0002/Caneta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Caneta0002
+{
+    public class Caneta
+    {
+        private string _corTinta;
+        private string _corDoCorpo;
+        private string _marca;
+        private string _modelo;
+
+        public Caneta(string corTinta, string corCorpo, string marca, string modelo)
+        {
+            _corTinta = corTinta;
+            _corDoCorpo = corCorpo;
+            _marca = marca;
+            _modelo = modelo;
+        }
+    }
+}
